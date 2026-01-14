c4a333b583951b38e7e19f0095ab2246c8aa9f84
d3vvvvvvvvvv <devpratap20567@gmail.com>
2026-01-14 19:47:58 +0530

    Sheet 2 level 2 problem 3

---
+++ Basic_programs/Square.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Square {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double perimeter = sc.nextDouble();
+        System.out.println("The length of the side is " + side + " whose perimeter is " + perimeter);
+    }
+}
