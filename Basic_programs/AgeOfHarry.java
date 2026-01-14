af8fcdfa16b37ed06989926aba8606c202dfd968
d3vvvvvvvvvv <devpratap20567@gmail.com>
2026-01-14 13:19:37 +0530

    sheet 2 problem 1

--- Basic_programs/AgeOfHarry.java
+++ Basic_programs/AgeOfHarry.java
@@ -1,9 +1,9 @@
-class AgeCalculator {
+public class AgeOfHarry {
     public static void main(String[] args) {
-        int birthYear = 2000;
+       int birthYear = 2000;
         int currentYear = 2024;
 
-        int age = currentYear - birthYear;
+       int age = currentYear - birthYear;
 
         System.out.println("Harry's age in 2024 is " + age);
     }
