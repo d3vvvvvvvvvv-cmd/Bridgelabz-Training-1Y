645cbde3a3c7a2d766ff255d32e363daae34752c
d3vvvvvvvvvv <devpratap20567@gmail.com>
2026-01-14 18:30:04 +0530

    sheet 2 problem 12

---
+++ Basic_programs/TriangleArea.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+          double base = sc.nextDouble();
+        double height = sc.nextDouble();
+          double areaInInches = 0.5 * base * height;
+        double areaInCm = areaInInches * 2.54 * 2.54;
+          System.out.println("Area in square inches = " + areaInInches);
+        System.out.println("Area in square centimeters = " + areaInCm);
+    }
+}
