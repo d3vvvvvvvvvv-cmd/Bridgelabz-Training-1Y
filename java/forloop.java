7aacf500e8c10eb4ee8f91dc8a1cd758db58ed64
d3vvvvvvvvvv <devpratap20567@gmail.com>
2026-01-22 23:13:31 +0530

    Workshop created

---
+++ java/forloop.java
@@ -0,0 +1,29 @@
+public class forloop{
+
+public static void main(String[] args){
+	int i=0;
+	System.out.println("val :" +i);
+	i++;
+	System.out.println("val a: " + i++);
+	System.out.println("val b: " +i);
+	++i;
+	System.out.println("val c: " + i++);
+	System.out.println("val d: " + i++);
+	
+	
+	
+    for (i=0;i<10;i++){
+	
+	System.out.println("i:" +i);
+	
+	}
+	
+	int j=10;
+	while(j>0){
+		System.out.println("j:" +j);
+		j--;
+	}
+	
+	
+}
+}
