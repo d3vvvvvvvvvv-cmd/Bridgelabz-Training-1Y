45383c373132950860ce80506ffbddbfb5307a28
d3vvvvvvvvvv <devpratap20567@gmail.com>
2026-01-27 23:18:52 +0530

    assignment 3 SumNum

---
+++ Basic_programs/SumNum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner ;
+public class SumNum{
+public static void main(String[] args){
+Scanner sc =  new Scanner(System.in);
+System.out.println("Enter 2 Numbers " );
+int num1 = sc.nextInt();
+int num2 = sc.nextInt();
+int sum =  num1 + num2 ;
+System.out.println("The Sum of two numbers is " + sum);
+}
+}
